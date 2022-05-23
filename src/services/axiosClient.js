76f281f1415f8157d4db85a443cd7fb0de87262d
hlnhuttoan <hlnhuttoan@gmail.com>
2022-05-23 14:47:26 +0700

    new change

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosOptions = {
   baseURL: "https://kmin-ecommerce-be.herokuapp.com",
+  timeout: "5000",
   headers: {
     "Content-Type": "application/json",
   },
