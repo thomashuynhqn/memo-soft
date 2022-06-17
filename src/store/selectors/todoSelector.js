d69cd13781c70048904e65d17289647fa25d19ca
toantommy9999 <hlnhuttoan@gmail.com>
2022-06-17 07:54:58 +0700

    new change

--- src/store/selectors/todoSelector.js
+++ src/store/selectors/todoSelector.js
@@ -1,23 +1,32 @@
 /* eslint-disable no-nested-ternary */
-import { createSelector } from '@reduxjs/toolkit'
+import { createSelector } from "@reduxjs/toolkit";
 
-const selectTodo = state => state.todo
+const selectTodo = (state) => state.todo;
 
-const selectTodos = createSelector(selectTodo, todo => todo.todos)
+const selectTodos = createSelector(selectTodo, (todo) => todo.todos);
 
-const selectTodoUpdate = createSelector(selectTodo, todo => todo.todoUpdate)
+const selectTodoUpdate = createSelector(selectTodo, (todo) => todo.todoUpdate);
 
-const selectFilter = createSelector(selectTodo, todo => todo.filter)
+const selectFilter = createSelector(selectTodo, (todo) => todo.filter);
 
-const selectFilteredTodos = createSelector(selectTodos, selectFilter, (todos, filter) =>
-	todos.filter(todo => (filter === 'All' ? true : filter === 'Completed' ? todo.isCompleted : !todo.isCompleted))
-)
+const selectFilteredTodos = createSelector(
+  selectTodos,
+  selectFilter,
+  (todos, filter) =>
+    todos.filter((todo) =>
+      filter === "All"
+        ? true
+        : filter === "Completed"
+        ? todo.isCompleted
+        : !todo.isCompleted
+    )
+);
 
 const todoSelector = {
-	selectTodos,
-	selectFilteredTodos,
-	selectTodoUpdate,
-	selectFilter
-}
+  selectTodos,
+  selectFilteredTodos,
+  selectTodoUpdate,
+  selectFilter,
+};
 
-export default todoSelector
+export default todoSelector;
